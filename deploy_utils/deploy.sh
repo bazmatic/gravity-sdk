c6fe3f1f08e976f324d314394c80a699cf0b3a4d
AlexYue <yj976240184@qq.com>
2024-09-06 14:41:32 +0800

    [chore] Extract deploy util dir to deposit all the shell (#13)

---
+++ deploy_utils/deploy.sh
@@ -0,0 +1,17 @@
+source env.sh
+
+echo ${ROOT_PATH}
+log_suffix=$(date +"%Y-%d-%m:%H:%M:%S")
+
+function start_node() {
+    node_path=$1
+    port=$2
+    node_path=${ROOT_PATH}/${node_path}
+    # temporarily set these two round to zero
+    jq 'walk(if type == "object" and (.last_voted_round? // .highest_timeout_round?) then .last_voted_round |= 0 | .highest_timeout_round |= 0 else . end)' ${node_path}/data/secure_storage.json > secure_storage.json
+    mv secure_storage.json ${node_path}/data/
+    ${ROOT_PATH}/target/debug/gravity_consensus --data-path ${node_path}/data --port ${port} --discovery-path ${node_path}/discovery > logs/${port}.${log_suffix}.log &
+}
+
+start_node node1 2024
+start_node node2 6180
