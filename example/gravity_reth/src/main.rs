bc9a630034a1d22245192746e918c44dd5387790
Acat <23625303+keanji-x@users.noreply.github.com>
2024-09-10 17:51:58 +0800

    [feat]: add reth example (#23)

---
+++ example/gravity_reth/src/main.rs
@@ -0,0 +1,54 @@
+mod cli;
+
+use clap::Parser;
+use reth_node_builder::EngineNodeLauncher;
+use reth_node_core::args::utils::DefaultChainSpecParser;
+use reth_node_ethereum::EthereumNode;
+use reth_node_ethereum::node::EthereumAddOns;
+use reth_provider::providers::BlockchainProvider2;
+use crate::cli::Cli;
+use clap::Args;
+
+#[derive(Debug, Clone, Args, PartialEq, Eq, Default)]
+#[command(next_help_heading = "Engine")]
+pub struct EngineArgs {
+    /// Enable the engine2 experimental features on reth binary
+    #[arg(long = "engine.experimental", default_value = "false")]
+    pub experimental: bool,
+}
+
+fn main() {
+    reth_cli_util::sigsegv_handler::install();
+    if std::env::var_os("RUST_BACKTRACE").is_none() {
+        std::env::set_var("RUST_BACKTRACE", "1");
+    }
+    if let Err(err) =
+        Cli::<DefaultChainSpecParser, EngineArgs>::parse().run(|builder, engine_args| async move {
+            let enable_engine2 = engine_args.experimental;
+            match enable_engine2 {
+                true => {
+                    let handle = builder
+                        .with_types_and_provider::<EthereumNode, BlockchainProvider2<_>>()
+                        .with_components(EthereumNode::components())
+                        .with_add_ons::<EthereumAddOns>()
+                        .launch_with_fn(|builder| {
+                            let launcher = EngineNodeLauncher::new(
+                                builder.task_executor().clone(),
+                                builder.config().datadir(),
+                            );
+                            builder.launch_with(launcher)
+                        })
+                        .await?;
+                    handle.node_exit_future.await
+                }
+                false => {
+                    let handle = builder.launch_node(EthereumNode::default()).await?;
+                    handle.node_exit_future.await
+                }
+            }
+        })
+    {
+        eprintln!("Error: {err:?}");
+        std::process::exit(1);
+    }
+}
