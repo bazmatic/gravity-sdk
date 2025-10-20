c41db5b92611df453c123b1611d405244f7ba907
nekomoto911 <nekomoto911@gmail.com>
2025-10-21 00:11:24 +0800

    fix: fix `BatchRequesterState::next_request_peers` (#415)

--- aptos-core/consensus/src/quorum_store/batch_requester.rs
+++ aptos-core/consensus/src/quorum_store/batch_requester.rs
@@ -4,21 +4,21 @@
 use crate::{
     monitor,
     network::QuorumStoreSender,
-    quorum_store::{
-        types::{BatchRequest, BatchResponse, PersistedValue},
-    },
+    quorum_store::types::{BatchRequest, BatchResponse, PersistedValue},
 };
 use aptos_consensus_types::proof_of_store::BatchInfo;
-use gaptos::aptos_config::network_id::{NetworkId, PeerNetworkId};
-use gaptos::aptos_crypto::HashValue;
 use aptos_executor_types::*;
-use gaptos::aptos_logger::prelude::*;
-use gaptos::aptos_types::{transaction::SignedTransaction, validator_verifier::ValidatorVerifier, PeerId};
 use futures::{stream::FuturesUnordered, StreamExt};
+use gaptos::{
+    aptos_config::network_id::{NetworkId, PeerNetworkId},
+    aptos_consensus::quorum_store::counters,
+    aptos_crypto::HashValue,
+    aptos_logger::prelude::*,
+    aptos_types::{transaction::SignedTransaction, validator_verifier::ValidatorVerifier, PeerId},
+};
 use rand::Rng;
 use std::{sync::Arc, time::Duration};
 use tokio::{sync::oneshot, time};
-use gaptos::aptos_consensus::quorum_store::counters as counters;
 
 struct BatchRequesterState {
     signers: Vec<PeerId>,
@@ -34,40 +34,35 @@ impl BatchRequesterState {
         ret_tx: oneshot::Sender<ExecutorResult<Vec<SignedTransaction>>>,
         retry_limit: usize,
     ) -> Self {
-        Self {
-            signers,
-            next_index: 0,
-            ret_tx,
-            num_retries: 0,
-            retry_limit,
-        }
+        Self { signers, next_index: 0, ret_tx, num_retries: 0, retry_limit }
     }
 
     fn next_request_peers(&mut self, num_peers: usize) -> Option<Vec<PeerId>> {
-        if self.signers.len() <= num_peers {
-            return Some(self.signers.clone());
-        }
-
         if self.num_retries == 0 {
             let mut rng = rand::thread_rng();
             // make sure nodes request from the different set of nodes
             self.next_index = rng.gen::<usize>() % self.signers.len();
-            counters::SENT_BATCH_REQUEST_COUNT.inc_by(num_peers as u64);
+            counters::SENT_BATCH_REQUEST_COUNT.inc_by(num_peers.min(self.signers.len()) as u64);
         } else {
-            counters::SENT_BATCH_REQUEST_RETRY_COUNT.inc_by(num_peers as u64);
+            counters::SENT_BATCH_REQUEST_RETRY_COUNT
+                .inc_by(num_peers.min(self.signers.len()) as u64);
         }
         if self.num_retries < self.retry_limit {
             self.num_retries += 1;
-            let ret = self
-                .signers
-                .iter()
-                .cycle()
-                .skip(self.next_index)
-                .take(num_peers)
-                .cloned()
-                .collect();
-            self.next_index = (self.next_index + num_peers) % self.signers.len();
-            Some(ret)
+            if self.signers.len() <= num_peers {
+                Some(self.signers.clone())
+            } else {
+                let ret = self
+                    .signers
+                    .iter()
+                    .cycle()
+                    .skip(self.next_index)
+                    .take(num_peers)
+                    .cloned()
+                    .collect();
+                self.next_index = (self.next_index + num_peers) % self.signers.len();
+                Some(ret)
+            }
         } else {
             None
         }
@@ -75,26 +70,12 @@ impl BatchRequesterState {
 
     fn serve_request(self, digest: HashValue, maybe_payload: Option<Vec<SignedTransaction>>) {
         if let Some(payload) = maybe_payload {
-            trace!(
-                "QS: batch to oneshot, digest {}, tx {:?}",
-                digest,
-                self.ret_tx
-            );
+            trace!("QS: batch to oneshot, digest {}, tx {:?}", digest, self.ret_tx);
             if self.ret_tx.send(Ok(payload)).is_err() {
-                debug!(
-                    "Receiver of requested batch not available for digest {}",
-                    digest
-                )
+                debug!("Receiver of requested batch not available for digest {}", digest)
             };
-        } else if self
-            .ret_tx
-            .send(Err(ExecutorError::CouldNotGetData))
-            .is_err()
-        {
-            debug!(
-                "Receiver of requested batch not available for unavailable digest {}",
-                digest
-            );
+        } else if self.ret_tx.send(Err(ExecutorError::CouldNotGetData)).is_err() {
+            debug!("Receiver of requested batch not available for unavailable digest {}", digest);
         }
     }
 }
@@ -146,20 +127,19 @@ impl<T: QuorumStoreSender + Sync + 'static> BatchRequester<T> {
         let available_peers = if self.my_peer_id.network_id() == NetworkId::Validator {
             signers
         } else {
-            self
-            .network_sender
-            .get_available_peers()
-            .map(|peers| {
-                peers
-                    .iter()
-                    .filter(|peer| peer.network_id() == self.my_peer_id.network_id())
-                    .map(|peer| peer.peer_id())
-                    .collect::<Vec<_>>()
-            })
-            .unwrap_or_else(|e| {
-                error!("Failed to get available peers: {:?}", e);
-                vec![]
-            })
+            self.network_sender
+                .get_available_peers()
+                .map(|peers| {
+                    peers
+                        .iter()
+                        .filter(|peer| peer.network_id() == self.my_peer_id.network_id())
+                        .map(|peer| peer.peer_id())
+                        .collect::<Vec<_>>()
+                })
+                .unwrap_or_else(|e| {
+                    error!("Failed to get available peers: {:?}", e);
+                    vec![]
+                })
         };
         if available_peers.is_empty() {
             warn!("No available peers to request batch");
